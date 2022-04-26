0bb3ac65750f0a51c1d0b8e1dc5a6ac0dd184c47
Shaneel Kumar <shaneel_kumar@live.com>
2022-04-26 21:31:05 +1200

    Started working in different modules and managed to setup a CSS file properly in terms of working with a bundler like webpack.

---
+++ src/front-page.js
@@ -0,0 +1,51 @@
+// Generating the front page of the restaurant.
+function frontPage() {
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header');
+    
+
+
+    const headerBanner = document.createElement('h1');
+    headerBanner.classList.add('banner');
+
+    const headerTitle = document.createElement('h1');
+    headerTitle.innerText = 'Test Kitchen';
+    headerTitle.classList.add('titleText');
+    headerBanner.appendChild(headerTitle);
+
+    const navContainer = document.createElement('div');
+    navContainer.classList.add('nav-container')
+
+    // Options to simulate tabbed navigation. 
+    const mainLink = document.createElement('a');
+    const mainTitle = document.createTextNode('Main');
+    mainLink.appendChild(mainTitle);
+    mainLink.title = 'Main';
+    mainLink.href = './';
+    mainLink.classList.add('titleText');
+    navContainer.appendChild(mainLink)
+    
+    const menuLink = document.createElement('a');
+    const menuTitle = document.createTextNode('Menu');
+    menuLink.appendChild(menuTitle);
+    menuLink.title = 'Menu';
+    menuLink.href = './';
+    menuLink.classList.add('titleText');
+    navContainer.appendChild(menuLink)
+    
+    const contactLink = document.createElement('a');
+    const contactTitle = document.createTextNode('Contact');
+    contactLink.appendChild(contactTitle);
+    contactLink.title = 'Contact';
+    contactLink.href = './';
+    contactLink.classList.add('titleText');
+    navContainer.appendChild(contactLink)
+    
+
+    headerContainer.appendChild(headerBanner);
+    headerContainer.appendChild(navContainer);
+
+    return headerContainer
+};
+
+export default frontPage
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import frontPage from "./front-page"
+import './style.css'
+
+const contentContainer = document.getElementById('contentContainer')
+
+contentContainer.appendChild(frontPage())
